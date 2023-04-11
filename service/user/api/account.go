0049d57212cf168f2ebf7945f9f49bce91f337b5
Ke Chen <2697947313@qq.com>
2023-04-11 11:16:22 +0800

    init: user service docs

--- service/user/api/account.go
+++ service/user/api/account.go
@@ -1,22 +1,39 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	_ "ChatDanBackend/common"
+	"github.com/gofiber/fiber/v2"
+)
 
 // Login godoc
 // @Summary Login
 // @Description Login
 // @Tags Account
-// @Accept  json
-// @Produce  json
-// @Param json body LoginRequest true "json"
+// @Accept json
+// @Produce json
+// @Router /api/user/login [post]
+// @Param body query LoginRequest true "The two fields are required, you can also add other fields(e.g. email)."
 // @Success 200 {object} common.Response{data=UserResponse}
 // @Failure 400 {object} common.Response "Bad Request"
 // @Failure 404 {object} common.Response "User Not Found"
 // @Failure 500 {object} common.Response "Internal Server Error"
 func Login(c *fiber.Ctx) error {
+	// todo: set cookie
 	return c.JSON(nil)
 }
 
+// Register godoc
+// @Summary Register
+// @Description Register
+// @Tags Account
+// @Accept json
+// @Produce json
+// @Router /api/user/register [post]
+// @Param body query LoginRequest true "The two fields are required, you can also add other fields(e.g. email)."
+// @Success 200 {object} common.Response{data=UserResponse}
+// @Failure 400 {object} common.Response "Bad Request"
+// @Failure 500 {object} common.Response "Internal Server Error"
 func Register(c *fiber.Ctx) error {
+	// todo: set cookie
 	return c.JSON(nil)
 }
