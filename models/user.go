49fd5593452b5694d8b2d2ba859ba57999e85fe9
Ke Chen <JingYiJun3104@outlook.com>
2023-05-09 23:01:22 +0800

    fix: gorm.DeletedAt type; 全怪 copilot

--- models/user.go
+++ models/user.go
@@ -1,15 +1,18 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
-	ID             int       `json:"id"`
-	Username       string    `json:"username" gorm:"index,size:30"`
-	Email          string    `json:"email" gorm:"index"`
-	HashedPassword string    `json:"-" gorm:"size:256"`
-	LoginTime      time.Time `json:"-" gorm:"autoUpdateTime"`
-	RegisterTime   time.Time `json:"-" gorm:"autoCreateTime"`
-	Banned         bool      `json:"banned"`
-	IsAdmin        bool      `json:"is_admin"`
-	DeletedAt      time.Time `json:"-"`
+	ID             int            `json:"id"`
+	Username       string         `json:"username" gorm:"index,size:30"`
+	Email          string         `json:"email" gorm:"index"`
+	HashedPassword string         `json:"-" gorm:"size:256"`
+	LoginTime      time.Time      `json:"-" gorm:"autoUpdateTime"`
+	RegisterTime   time.Time      `json:"-" gorm:"autoCreateTime"`
+	Banned         bool           `json:"banned"`
+	IsAdmin        bool           `json:"is_admin"`
+	DeletedAt      gorm.DeletedAt `json:"-"`
 }
--- models/wall.go
+++ models/wall.go
@@ -1,16 +1,17 @@
 package models
 
 import (
+	"gorm.io/gorm"
 	"time"
 )
 
 type Wall struct {
-	ID         int        `json:"id"`
-	CreatedAt  time.Time  `json:"created_at" gorm:"index"`
-	UpdatedAt  time.Time  `json:"updated_at" gorm:"index"`
-	DeletedAt  *time.Time `json:"-" gorm:"index"`
-	PosterID   int        `json:"poster_id"`
-	Poster     *User      `json:"-" gorm:"foreignKey:PosterID"`
-	Content    string     `json:"content"`
-	Visibility string     `json:"visibility"`
+	ID         int            `json:"id"`
+	CreatedAt  time.Time      `json:"created_at" gorm:"index"`
+	UpdatedAt  time.Time      `json:"updated_at" gorm:"index"`
+	DeletedAt  gorm.DeletedAt `json:"-" gorm:"index"`
+	PosterID   int            `json:"poster_id"`
+	Poster     *User          `json:"-" gorm:"foreignKey:PosterID"`
+	Content    string         `json:"content"`
+	Visibility string         `json:"visibility"`
 }
